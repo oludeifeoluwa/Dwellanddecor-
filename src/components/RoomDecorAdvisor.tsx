2ebf44c4fb57b83587b46e702405db4df92bba55
oludeifeoluwa <Bioes2007@gmail.com>
2026-08-14 22:43:27 +0100

    Update image paths for RoomDecorAdvisor component

--- src/components/RoomDecorAdvisor.tsx
+++ src/components/RoomDecorAdvisor.tsx
@@ -25,8 +25,8 @@ import { handleImageError } from '../utils/imageHelper';
 // Public room aesthetic images served from /public/images/
 const cozySunsetNeonImg = '/images/IMG-20260806-WA0188(1).jpg';
 const cyberSunsetRoomImg = '/images/IMG-20260806-WA0187.jpg';
-const neonFlexWaveImg = '/images/neon_flex_wave.jpg';
-const fairyLightBotanicalImg = '/images/fairy_light_botanical.jpg';
+const neonFlexWaveImg = '/images/IMG-20260806-WA0128.jpg';
+const fairyLightBotanicalImg = '/images/IMG-20260806-WA0189.jpg';
 const popMusicWallImg = '/images/pop_music_wall.jpg';
 const ultimateDormGlowupImg = '/images/ultimate_dorm_glowup.jpg';
 
