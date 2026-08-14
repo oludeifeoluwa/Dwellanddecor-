4ee8d7c5a135db9cd2e56f9f9fd11cdcafef3367
oludeifeoluwa <Bioes2007@gmail.com>
2026-08-14 19:50:51 +0100

    Update cozy sunset neon image path

--- src/components/RoomDecorAdvisor.tsx
+++ src/components/RoomDecorAdvisor.tsx
@@ -23,7 +23,7 @@ import { useShop } from '../context/ShopContext';
 import { handleImageError, getCleanImageUrl } from '../utils/imageHelper';
 
 // Public room aesthetic images served from /public/images/
-const cozySunsetNeonImg = '/images/cozy_sunset_neon.jpg';
+const cozySunsetNeonImg = '/images/IMG-20260806-WA0188(1).jpg';
 const cyberSunsetRoomImg = '/images/cyber_sunset_room.jpg';
 const neonFlexWaveImg = '/images/neon_flex_wave.jpg';
 const fairyLightBotanicalImg = '/images/fairy_light_botanical.jpg';
