c7f28aef1f177203104b75299135ae5290daf90b
oludeifeoluwa <Bioes2007@gmail.com>
2026-08-15 11:24:24 -0700

    v

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    build: {
+    sourcemap: true, // <-- Add this line!
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
